95bf1b462d23a52ef89078a3fd8de2f601bd5e63
Dal S Garbuja Pun <dsgarbujapun@gmail.com>
2021-09-25 07:39:04 +1000

    Initial commit

---
+++ classifieds.py
@@ -0,0 +1,225 @@
+
+#-----Statement of Authorship----------------------------------------#
+#
+#  This is an individual assessment item for QUT's teaching unit
+#  IFB104, "Building IT Systems", Semester 2, 2021.  By submitting
+#  this code I agree that it represents my own work.  I am aware of
+#  the University rule that a student must not act in a manner
+#  which constitutes academic dishonesty as stated and explained
+#  in QUT's Manual of Policies and Procedures, Section C/5.3
+#  "Academic Integrity" and Section E/2.1 "Student Code of Conduct".
+#
+student_number = None # put your student number here as an integer
+student_name   = None # put your name here as a character string
+#
+#  NB: Files submitted without a completed copy of this statement
+#  will not be marked.  All files submitted will be subjected to
+#  software plagiarism analysis using the MoSS system
+#  (http://theory.stanford.edu/~aiken/moss/).
+#
+#--------------------------------------------------------------------#
+
+
+
+#-----Assignment Description-----------------------------------------#
+#
+#  Classified Ads
+#
+#  In this assignment you will combine your knowledge of HTMl/CSS
+#  mark-up languages with your skills in Python scripting, pattern
+#  matching, databases and Graphical User Interface design to produce
+#  a robust, interactive application that allows its user to view
+#  and save items currently for sale from multiple online sources.
+#
+#  See the client's requirements accompanying this file for full
+#  details.
+#
+#--------------------------------------------------------------------#
+
+
+
+#-----Initialisation Steps-------------------------------------------#
+#
+
+# Import standard Python 3 modules needed to complete this assignment.
+# You should not need to use any modules other than those provided
+# in a standard Python 3 installation for your solution.
+#
+# In particular, you may NOT use any Python modules that need to be
+# downloaded and installed separately, such as "Beautiful Soup" or
+# "Pillow", because the markers will not have access to such modules
+# and will not be able to run your code.  Only modules that are part
+# of a standard Python 3 installation may be used.
+
+# A function for exiting the program immediately (renamed
+# because "exit" is already a standard Python function).
+from sys import exit as abort
+
+# A function for opening a web document given its URL.
+# (You WILL need to use this function in your solution,
+# either directly or via the "download" function below.)
+from urllib.request import urlopen
+
+# Some standard Tkinter functions.  (You WILL need to use
+# SOME of these functions in your solution.)  You may also
+# import other widgets from the "tkinter" module, provided they
+# are standard ones and don't need to be downloaded and installed
+# separately.  (NB: Although you can import individual widgets
+# from the "tkinter.tkk" module, DON'T import ALL of them
+# using a "*" wildcard because the "tkinter.tkk" module
+# includes alternative versions of standard widgets
+# like "Label".)
+from tkinter import *
+from tkinter.scrolledtext import ScrolledText
+from tkinter.ttk import Progressbar
+
+# Functions for finding occurrences of a pattern defined
+# via a regular expression.  (You do not necessarily need to
+# use these functions in your solution, because the problem
+# can be solved with the string "find" function, but it will
+# be difficult to produce a concise and robust solution
+# without using regular expressions.)
+from re import *
+
+# A function for displaying a web document in the host
+# operating system's default web browser (renamed to
+# distinguish it from the built-in "open" function for
+# opening local files).  (You WILL need to use this function
+# in your solution.)
+from webbrowser import open as urldisplay
+
+# All the standard SQLite functions.
+from sqlite3 import *
+
+# Confirm that the student has declared their authorship.
+# You must NOT change any of the code below.
+if not isinstance(student_number, int):
+    print('\nUnable to run: No student number supplied',
+          '(must be an integer)\n')
+    abort()
+if not isinstance(student_name, str):
+    print('\nUnable to run: No student name supplied',
+          '(must be a character string)\n')
+    abort()
+
+#
+#--------------------------------------------------------------------#
+
+
+
+#-----Supplied Function----------------------------------------------#
+#
+# Below is a function you can use in your solution if you find it
+# helpful.  (You are not required to use this function, but it may
+# save you some effort.)
+#
+
+# A function to download and save a web document.  The function
+# returns the downloaded document as a character string and
+# optionally saves it as a local file.  If the attempted download
+# fails, an error message is written to the shell window and the
+# special value None is returned.
+#
+# Parameters:
+# * url - The address of the web page you want to download.
+# * target_filename - Name of the file to be saved (if any).
+# * filename_extension - Extension for the target file, usually
+#      "html" for an HTML document or "xhtml" for an XML
+#      document.
+# * save_file - A file is saved only if this is True. WARNING:
+#      The function will silently overwrite the target file
+#      if it already exists!
+# * char_set - The character set used by the web page, which is
+#      usually Unicode UTF-8, although some web pages use other
+#      character sets.
+# * incognito - If this parameter is True the Python program will
+#      try to hide its identity from the web server. This can
+#      sometimes be used to prevent the server from blocking access
+#      to Python programs. However we discourage using this
+#      option as it is both unreliable and unethical to
+#      override the wishes of the web document provider!
+#
+def download(url = 'http://www.wikipedia.org/',
+             target_filename = 'downloaded_document',
+             filename_extension = 'html',
+             save_file = True,
+             char_set = 'UTF-8',
+             incognito = False):
+
+    # Import the function for opening online documents and
+    # the class for creating requests
+    from urllib.request import urlopen, Request
+
+    # Import an exception raised when a web server denies access
+    # to a document
+    from urllib.error import HTTPError
+
+    # Open the web document for reading
+    try:
+        if incognito:
+            # Pretend to be a web browser instead of
+            # a Python script (NOT RELIABLE OR RECOMMENDED!)
+            request = Request(url)
+            request.add_header('User-Agent',
+                               'Mozilla/5.0 (Windows NT 10.0; Win64; x64) ' +
+                               'AppleWebKit/537.36 (KHTML, like Gecko) ' +
+                               'Chrome/42.0.2311.135 Safari/537.36 Edge/12.246')
+            print("Warning - Request does not reveal client's true identity.")
+            print("          This is both unreliable and unethical!")
+            print("          Proceed at your own risk!\n")
+        else:
+            # Behave ethically
+            request = url
+        web_page = urlopen(request)
+    except ValueError:
+        print("Download error - Cannot find document at URL '" + url + "'\n")
+        return None
+    except HTTPError:
+        print("Download error - Access denied to document at URL '" + url + "'\n")
+        return None
+    except Exception as message:
+        print("Download error - Something went wrong when trying to download " + \
+              "the document at URL '" + url + "'")
+        print("Error message was:", message, "\n")
+        return None
+
+    # Read the contents as a character string
+    try:
+        web_page_contents = web_page.read().decode(char_set)
+    except UnicodeDecodeError:
+        print("Download error - Unable to decode document from URL '" + \
+              url + "' as '" + char_set + "' characters\n")
+        return None
+    except Exception as message:
+        print("Download error - Something went wrong when trying to decode " + \
+              "the document from URL '" + url + "'")
+        print("Error message was:", message, "\n")
+        return None
+
+    # Optionally write the contents to a local text file
+    # (overwriting the file if it already exists!)
+    if save_file:
+        try:
+            text_file = open(target_filename + '.' + filename_extension,
+                             'w', encoding = char_set)
+            text_file.write(web_page_contents)
+            text_file.close()
+        except Exception as message:
+            print("Download error - Unable to write to file '" + \
+                  target_filename + "'")
+            print("Error message was:", message, "\n")
+
+    # Return the downloaded document to the caller
+    return web_page_contents
+
+#
+#--------------------------------------------------------------------#
+
+
+
+#-----Student's Solution---------------------------------------------#
+#
+# Put your solution at the end of this file.
+#
+
+pass
